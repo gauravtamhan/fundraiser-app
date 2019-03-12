222c988fee9e9402531e95fcfa9e0200fe57ccb7
Gaurav Tamhan <gaurav.tamhan@gmail.com>
2019-03-12 17:59:50 -0400

    Added categories and a new date/time picker for creating tasks.

---
+++ src/assets/categories.js
@@ -0,0 +1,37 @@
+const categories = [
+    {
+        "name": "Assistance",
+    },
+    {
+        "name": "Care",
+    },
+    {
+        "name": "Cleaning",
+    },
+    {
+        "name": "Construction",
+    },
+    {
+        "name": "Errands",
+    },
+    {
+        "name": "House Work",
+    },
+    {
+        "name": "Manual Labor",
+    },
+    {
+        "name": "Removal",
+    },
+    {
+        "name": "Shopping",
+    },
+    {
+        "name": "Yard Work",
+    },
+    {
+        "name": "Other",
+    },
+];
+
+export default categories
