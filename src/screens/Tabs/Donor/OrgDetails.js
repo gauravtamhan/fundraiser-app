d54d1c9255239d3f0946998f534c92dd543db53e
jonathan <jcclark43@gmail.com>
2019-04-12 14:03:02 -0400

    added percentage of donations complete

--- src/screens/Tabs/Donor/OrgDetails.js
+++ src/screens/Tabs/Donor/OrgDetails.js
@@ -4,6 +4,7 @@ import { Button, Text, Content } from 'native-base';
 import { auth, database, provider } from '@src/firebase';
 import { THEME_COLOR } from '@assets/colors';
 import styles from '@assets/styles';
+import ProgressCircle from 'react-native-progress-circle'
 
 
 export default class OrgDetails extends Component {
@@ -27,11 +28,23 @@ export default class OrgDetails extends Component {
         console.log(org)
 
         return (
+            <View>
+            <ProgressCircle
+            percent={30}
+            radius={50}
+            borderWidth={8}
+            color="#3399FF"
+            shadowColor="#999"
+            bgColor="#fff"
+            >
+                <Text style={{ fontSize: 18 }}>{'30%'}</Text>
+            </ProgressCircle>
             <Content showsVerticalScrollIndicator={false}>
                 <View>
                     <Text>{org.name}</Text>
                 </View>
             </Content>
+            </View>
         );
     }
 }
